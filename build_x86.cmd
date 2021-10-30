190bc1f13e05adffcae2ca75eb75695630a81b37
Yu Chen <yu.chen@live.ie>
2021-10-30 16:59:44 +0800

    .

--- build_x86.cmd
+++ build_x86.cmd
@@ -1,2 +1,3 @@
-conan install %1@ -s arch=x86 -s compiler.runtime=MT --build %2 %3 %4 %5 %6 %7 %8 %9
+echo conan install %1@ -s arch=x86 -s compiler.runtime=MT --build %2
+conan install %1@ -s arch=x86 -s compiler.runtime=MT --build %2
 exit 0
