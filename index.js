b3c7dc91d773b331a4081af582720db0ca427ca7
Yu Chen <yu.chen@live.ie>
2021-10-30 21:17:02 +0800

    .

--- index.js
+++ index.js
@@ -2,50 +2,39 @@ const tar = require('tar');
 const fs = require('fs');
 const cp = require('child_process');
 
-const targets = {
-  x86: 'i386-pc-win32',
-  x86_64: 'x86_64-pc-windows-msvc',
-};
-
-let name = process.env.build_target + '-' + process.env.build_version
-if (process.env.name_suffix)
-  name += '-' + process.env.name_suffix;
 const reference = process.env.build_target + '/' + process.env.build_version;
 
-for (const arch of ['x86_64', 'x86']) {
-  const jsonPath = `${name}-${arch}.json`;
-  const cmdInstall = [
-    'conan',
-    'install',
-    reference + '@',
-    `-s arch=${arch}`,
-    '--build=missing',
-    process.env[arch + '_flags'],
-  ].join(' ');
-  console.log(cmdInstall);
-  cp.execSync(cmdInstall, { stdio: 'inherit' });
+const jsonPath = `${process.env.build_output_basename}.json`;
+const cmdInstall = [
+  'conan',
+  'install',
+  reference + '@',
+  '--build=missing',
+  process.env.build_flags,
+].join(' ');
+console.log(cmdInstall);
+cp.execSync(cmdInstall, { stdio: 'inherit' });
 
-  const cmdInfo = [
-    'conan',
-    'info',
-    reference + '@',
-    `-s arch=${arch}`,
-    process.env[arch + '_flags'],
-    `--paths --json ${jsonPath}`,
-  ].join(' ');
-  console.log(cmdInfo);
-  cp.execSync(cmdInfo, { stdio: 'inherit' });
+const cmdInfo = [
+  'conan',
+  'info',
+  reference + '@',
+  process.env.build_flags,
+  `--paths --json ${jsonPath}`,
+].join(' ');
+console.log(cmdInfo);
+cp.execSync(cmdInfo, { stdio: 'inherit' });
 
-  const jsonArray = JSON.parse(fs.readFileSync(jsonPath).toString());
-  const json = jsonArray.find(x => x.reference === reference);
-  console.log(json);
-  const cwd = json.package_folder;
-  const output = name + '-' + targets[arch] + '.tar.gz';
-  tar.c(
-    {
-      gzip: true,
-      cwd,
-    },
-    ['.']
-  ).pipe(fs.createWriteStream(output));
-}
+const jsonArray = JSON.parse(fs.readFileSync(jsonPath).toString());
+const json = jsonArray.find(x => x.reference === reference);
+console.log(json);
+const cwd = json.package_folder;
+const output = process.env.build_output_basename + '.tgz';
+tar.c(
+  {
+    gzip: true,
+    cwd,
+  },
+  ['.']
+).pipe(fs.createWriteStream(output));
+
