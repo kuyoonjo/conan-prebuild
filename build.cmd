b6d4b4a893b9aca0ebf162ebc2bb3c3308d5ac94
Yu Chen <yu.chen@live.ie>
2021-10-30 17:50:12 +0800

    .

--- build.cmd
+++
@@ -1,5 +0,0 @@
-echo conan install %2@ -s arch=%1 --build=missing %3
-conan install %2@ -s arch=%1 --build=missing %3
-conan info %2@ -s arch=%1 %3 --paths --json %1.json
-type %1.json
-exit 0
--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const tar = require('tar');
 const fs = require('fs');
+const cp = require('child_process');
 
 const targets = {
   x86: 'i386-pc-win32',
   x86_64: 'x86_64-pc-windows-msvc',
 };
 
-for (const arch of ['x86', 'x86_64']) {
+for (const arch of ['x86_64', 'x86']) {
+  const cmdInstall = [
+    'conan',
+    'install',
+    process.env.build_target,
+    `-s arch=${arch}`,
+    '--build=missing',
+    process.env[arch + '_flags'],
+  ].join(' ');
+  console.log(cmdInstall);
+  cp.execSync(cmdInstall, { stdio: 'inherit' });
+
+  const cmdInfo = [
+    'conan',
+    'info',
+    process.env.build_target,
+    `-s arch=${arch}`,
+    process.env[arch + '_flags'],
+    `--paths --json ${arch}.json`,
+  ].join(' ');
+  console.log(cmdInfo);
+  cp.execSync(cmdInfo, { stdio: 'inherit' });
+
   const json = JSON.parse(fs.readFileSync(arch + '.json').toString());
+  console.log(json);
   const cwd = json.package_folder;
   const name = json.reference.replace(/\//g, '-');
   tar.c(
