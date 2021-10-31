52bc2d1071e6c226b372b41232d87c46e52822a9
Yu Chen <yu.chen@live.ie>
2021-10-31 15:24:38 +0800

    .

--- prepare.js
+++ prepare.js
@@ -1,7 +1,7 @@
 const cp = require('child_process');
 
 try {
-  cp.execSync(`refreshenv && gh release create ${process.env.build_target} --notes "${process.env.build_target}"`, {
+  cp.execSync(`gh release create ${process.env.build_target} --notes "${process.env.build_target}"`, {
     stdio: 'inherit',
   });
 } catch { }
