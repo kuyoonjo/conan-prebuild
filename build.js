efd3d5b600140c1db6e65bbb482d75c1774eb8ac
Yu Chen <yu.chen@live.ie>
2021-11-04 16:24:53 +0800

    conan build v1

--- build.js
+++ build.js
@@ -2,22 +2,6 @@ const tar = require('tar');
 const fs = require('fs');
 const cp = require('child_process');
 
-if (process.env.build_deps) {
-  console.log('Build Deps:', process.env.build_deps);
-  const deps = process.env.build_deps.split(',');
-  for (const dep of deps) {
-    const cmdInstall = [
-      'conan',
-      'install',
-      dep + '@',
-      '--build=missing',
-      process.env.build_flags,
-    ].join(' ');
-    console.log(cmdInstall);
-    cp.execSync(cmdInstall, { stdio: 'inherit' });
-  }
-}
-
 const reference = process.env.build_target + '/' + process.env.build_version;
 
 const jsonPath = `${process.env.build_output_basename}.json`;
