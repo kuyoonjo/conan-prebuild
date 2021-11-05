bdd6599a21ce6a0b40a6856ff1dbf9f53fa39efd
Yu Chen <yu.chen@live.ie>
2021-11-05 22:35:19 +0800

    .

--- arm64-apple-darwin.cmake
+++ arm64-apple-darwin.cmake
@@ -1,10 +1,7 @@
 set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR arm64)
 
-set(arch arm64)
+set(triple arm64-apple-darwin)
 
-set(CMAKE_C_FLAGS -arch $arch)
-set(CMAKE_CXX_FLAGS -arch $arch)
-set(CMAKE_EXE_LINKER_FLAGS -arch $arch)
-set(CMAKE_MODULE_LINKER_FLAGS -arch $arch)
-set(CMAKE_SHARED_LINKER_FLAGS -arch $arch)
-set(CMAKE_STATIC_LINKER_FLAGS -arch $arch)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
